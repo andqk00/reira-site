73521f94561b56e8b6487775428ae52d9d62fb0e
An Do Quoc Khanh <doquockhanhan@gmail.com>
2025-04-19 17:54:36 +0900

    feat: add some ignore linting

--- src/actions/send-email.ts
+++ src/actions/send-email.ts
@@ -4,7 +4,6 @@ import React from "react";
 import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
-import { error } from "console";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
